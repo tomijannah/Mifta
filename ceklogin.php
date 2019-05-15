9121413df6440305a140b314c3b43a869a1ad940
rhiskaamaliarpl2 <rhaliapasklaber@gmail.com>
2019-05-15 10:53:04 +0700

    mifta

---
+++ ceklogin.php
@@ -0,0 +1,16 @@
+<?php 
+// mengaktifkan session php
+session_start();
+ 
+// menghubungkan dengan koneksi
+include 'koneksi.php';
+ 
+// menangkap data yang dikirim dari form
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+// menyeleksi data admin dengan username dan password yang sesuai
+$data = mysqli_query($koneksi,"select * from  bootstrap-3.3.7-dist where username='$username' and password='$password'");
+
+ 
+?>
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * $dbconnect : koneksi kedatabase
+ */
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASSWORD', '');
+define('DBNAME', 'form_login'); //NAMA DATABASE
+
+
+$dbconnect = new mysqli(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+/**
+ * Check Error yang terjadi saat koneksi
+ * jika terdapat error maka die() // stop dan tampilkan error
+ */
+if ($dbconnect->connect_error) {
+    die('Database Not Connect. Error : ' . $dbconnect->connect_error);
+}
+?>
